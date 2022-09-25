c018d367dc2487aac8b78405a7d970a391307c5b
lanomade <ganne.hannah@gmail.com>
2022-09-25 21:29:01 +0200

    add conditional styling

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,10 +1,13 @@
-export default function ListItem({title, list}) {
+export default function ListItem({ title, list, flexRow }) {
+    let style = {}
+    flexRow && (style = {flexDirection: 'row', fontSize: '0.8em'})
+
     return (
-        <>
+        <div className="footer-main-item">
             {title && <h3>{title}</h3>}
-            <ul>
+            <ul style={style}>
                 {list.map(item => <li key={item}><a key={item} href="#">{item}</a></li>)}
             </ul>
-        </>
+        </div>
     )
 }
