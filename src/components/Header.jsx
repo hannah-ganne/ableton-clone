336e20dd30010f53e7d7d17cc2320b2f9fe44b1f
lanomade <ganne.hannah@gmail.com>
2022-09-27 14:54:05 +0200

    add logic for sticky header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,47 @@
 import logo from '../assets/logo.svg'
 import NavList from './NavList'
+import { useEffect, useState } from 'react'
 
 export default function Header() {
+    const [topValue, setTopValue] = useState(0)
+
+
+    useEffect(() => {
+        const body = document.body
+        const scrollUp = "scroll-up"
+        const scrollDown = "scroll-down"
+        let lastScroll = 0;
+        
+        window.addEventListener("scroll", () => {
+            const currentScroll = window.pageYOffset;
+
+            if (currentScroll <= 83) {
+                body.classList.remove(scrollUp);
+
+                return;
+            }
+
+            // if (currentScroll <= 0) {
+            //     body.classList.remove(scrollUp);
+
+            //     return;
+            // }
+
+            if (currentScroll > lastScroll && !body.classList.contains(scrollDown)) {
+              // down
+                body.classList.remove(scrollUp);
+                body.classList.add(scrollDown);
+
+            } else if (currentScroll < lastScroll && body.classList.contains(scrollDown)) {
+              // up
+                body.classList.remove(scrollDown);
+                body.classList.add(scrollUp);
+            }
+
+            lastScroll = currentScroll;
+        });
+    })
+
     return (
         <header>
             <section>
@@ -12,7 +52,8 @@ export default function Header() {
                 <NavList list={['Try Live for free', 'Log in or register']} />
             </section>
             <div className="divider"></div>
-            <section>
+            <div className="container"></div>
+            <section className="header-secondary">
                 <NavList list={['About', 'Jobs', 'Apprenticeships']} />
             </section>
         </header>
